d5d4f0e86108f0071bdb8d394d2f3d1180fa8edc
MandeepSingh6 <101950170+MandeepSingh6@users.noreply.github.com>
2023-07-12 19:01:21 -0400

    initial theme set to dark

--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const themeSlice = createSlice({
   name: "theme",
-  initialState: false,
+  initialState: true,
   reducers: {
     toggle: (state) => !state,
   },
